1fc301148be5b320bf797d3e65b96393284ddc66
LucianoSigot <lucianosigot@gmail.com>
2025-05-30 09:54:33 -0300

    Update Virtual.java

--- project/src/main/java/logica/Virtual.java
+++ project/src/main/java/logica/Virtual.java
@@ -1,1 +1,49 @@
+package com.example;
+
+public class Virtual implements Memoria {
+    private static Virtual instancia = null;
+
+    protected int tamano;
+    protected Pagina[] arrayMemoria;
+
+    // Constructor privado
+    private Virtual(int tamanoTotal) {
+        this.tamano = Memoria.getTamanoArray(tamanoTotal);
+        this.arrayMemoria = new Pagina[this.tamano];
+    }
+
+    // Método estático para obtener la única instancia
+    public static Virtual getInstancia(int tamanoTotal) {
+        if (instancia == null) {
+            instancia = new Virtual(tamanoTotal);
+        }
+        return instancia;
+    }
+
+    @Override
+    public Pagina setPagina(int idL, Elemento elemento) {
+        Pagina nuevaPagina = new Pagina(idL, elemento);
+        for (int i = 0; i < arrayMemoria.length; i++) {
+            if (arrayMemoria[i] == null) {
+                arrayMemoria[i] = nuevaPagina;
+                return nuevaPagina;
+            }
+        }
+        throw new RuntimeException("Memoria Virtual llena, no se puede cargar más páginas.");
+    }
+
+    @Override
+    public Pagina getPagina(int idL) {
+        for (Pagina pagina : arrayMemoria) {
+            if (pagina != null && pagina.getIdL() == idL) {
+                return pagina;
+            }
+        }
+        return null;
+    }
+
+    public Pagina[] getArrayMemoria() {
+        return arrayMemoria;
+    }
+}
 

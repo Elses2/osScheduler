afe12a56dbad1b83e591f8edc896dde6f4d50fb7
LucianoSigot <lucianosigot@gmail.com>
2025-05-30 09:46:11 -0300

    Update Proceso.java

--- project/src/main/java/logica/Proceso.java
+++ project/src/main/java/logica/Proceso.java
@@ -1,1 +1,44 @@
+import java.util.*;
+
+public class Proceso {
+    private int idProceso;
+    private List<Elementos> elementos; //Composicion de elementos 
+
+    public Proceso(int idProceso) {
+        this.idProceso = idProceso;
+        this.elementos = new ArrayList<>();
+        cargarElementosPorDefecto(); //metodo llamado en el constructor para que se haga por defecto de manera statica
+    }
+
+    // Getter
+    public int getIdProceso() {
+        return idProceso;
+    }
+
+    // Carga automática de 10 elementos al crear el proceso
+    private void cargarElementosPorDefecto() {
+        for (int i = 1; i <= 10; i++) {
+            elementos.add(new Elementos(i));
+        }
+    }
+
+    public void agregarElemento(Elementos elemento) {
+        elementos.add(elemento);
+    }
+
+    public void eliminarElemento(Elementos elemento) {
+        elementos.remove(elemento);
+    }
+
+    public void mostrarElementos() { //Metodo que muestran los 10 elementos dentro del proceso
+        if (elementos.isEmpty()) {
+            System.out.println("No hay elementos en el proceso " + idProceso + ".");
+        } else {
+            System.out.println("Elementos del proceso " + idProceso + ":");
+            for (Elementos e : elementos) {
+                System.out.println(e);
+            }
+        }
+    }
+}
 

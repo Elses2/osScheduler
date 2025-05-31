92e7c6d8c13aaf28d938e430cd262716a282a9fb
Elses2 <eberchiecher2@gmail.com>
2025-05-31 09:50:20 -0300

    add puglin maven javafx + un ejemplo

--- project/src/main/java/menu/Main.java
+++ project/src/main/java/menu/Main.java
@@ -1,32 +1,51 @@
 package menu;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import javafx.collections.FXCollections;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        String[] datos = {"Uno", "Dos", "Tres", "Cuatro"};
-        ListView<String> lista = new ListView<>(FXCollections.observableArrayList(datos));
+        HBox root = new HBox(20); // contenedor horizontal
 
-        VBox root = new VBox(lista);
-        Scene escena = new Scene(root, 300, 200);
+        // Pila 1
+        VBox pila1 = new VBox(10);
+        pila1.getChildren().addAll(new Label("P1-A"), new Label("P1-B"));
 
-        // Cargar el CSS
-        escena.getStylesheets().add(getClass().getResource("/estilos.css").toExternalForm());
+        // Pila 2
+        VBox pila2 = new VBox(10);
+        pila2.getChildren().addAll(new Label("P2-A"), new Label("P2-B"), new Label("P2-C"));
 
-        stage.setScene(escena);
-        stage.setTitle("Lista con CSS");
+        // Matriz (2 columnas, 3 filas)
+        GridPane matriz = new GridPane();
+        matriz.setHgap(10);
+        matriz.setVgap(10);
+        String[][] datos = {
+            {"Fila 1 Col 1", "Fila 1 Col 2"},
+            {"Fila 2 Col 1", "Fila 2 Col 2"},
+            {"Fila 3 Col 1", "Fila 3 Col 2"}
+        };
+        for (int fila = 0; fila < datos.length; fila++) {
+            for (int col = 0; col < datos[fila].length; col++) {
+                matriz.add(new Label(datos[fila][col]), col, fila);
+            }
+        }
+
+        // Agregamos todo al HBox
+        root.getChildren().addAll(pila1, pila2, matriz);
+
+        // Mostrar escena
+        Scene scene = new Scene(root, 600, 300);
+        stage.setScene(scene);
+        stage.setTitle("2 Pilas + 1 Matriz");
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
 

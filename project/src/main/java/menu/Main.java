f5617348065853cd0437b196afa6fa4d7214c155
Elses2 <eberchiecher2@gmail.com>
2025-05-31 07:43:39 -0300

    add prueba

--- project/src/main/java/menu/Main.java
+++ project/src/main/java/menu/Main.java
@@ -1,17 +1,32 @@
 package menu;
 
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Mi App JavaFX");
-        primaryStage.show();
+    public void start(Stage stage) {
+        String[] datos = {"Uno", "Dos", "Tres", "Cuatro"};
+        ListView<String> lista = new ListView<>(FXCollections.observableArrayList(datos));
+
+        VBox root = new VBox(lista);
+        Scene escena = new Scene(root, 300, 200);
+
+        // Cargar el CSS
+        escena.getStylesheets().add(getClass().getResource("/estilos.css").toExternalForm());
+
+        stage.setScene(escena);
+        stage.setTitle("Lista con CSS");
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
 

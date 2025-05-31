6f7f7dc4c5b8456343af70ec0eca8e3f3ef39f3b
Elses2 <eberchiecher2@gmail.com>
2025-05-31 05:31:19 -0300

    agregar javafx al pom

--- project/src/main/java/menu/Main.java
+++ project/src/main/java/menu/Main.java
@@ -1,16 +1,17 @@
 package menu;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main{
-
-
-    public void main(String [] args){
-
-      System.out.println("Funciono");
-
-
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Mi App JavaFX");
+        primaryStage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
-
 
